b836618b380cc588e0102778c158d1555cefdb14
Mohd Swaleh Siddiqui <120578077+swaleh-siddiqui@users.noreply.github.com>
2023-07-21 19:39:33 +0530

    Add files via upload

---
+++ Employee_Management_System/Employee_Management_System/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for Employee_Management_System project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from employee.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name = "index"),
+    path('register', register, name = "register"),
+    path('emp_login', emp_login, name = "emp_login"),
+    path('emp_home', emp_home, name = "emp_home"),
+    path('profile', profile, name = "profile"),
+    path('logout', Logout, name = "logout"),
+    path('edit_profile', edit_profile, name = "edit_profile"),
+]
